be577e2a2e9b1a742725fc6d660a7fde3ddaeb4e
woochen001 <144323112+woochen001@users.noreply.github.com>
2026-06-15 01:22:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'railway.woovps.kdns.fr';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiODZkZGE4YzRmYmQ1YTcwY2U2NTRkYzZjMjVlNDUwNWYiLCJ0IjoiZDI5NzcwYWUtMjdkNy00OWRlLWJkZWUtNzQ2ZWQ3OWNiOTE3IiwicyI6Ik9UZzJZVFJsTVdRdE5UYzBPUzAwTmpjeExUaGlORFV0T1RSbU1XWTNNVEU0TURNMCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'saas.sin.fan';            // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'visa.com.hk';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || '';                        // 节点名称
 
